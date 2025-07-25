2667894efa74cee8cf02e9c3485b33e86ca5d3c2
Kira1922 <kirap996@gmail.com>
2025-07-26 01:03:45 +0300

    Добавлен виджет ноутбук

--- dogs.py
+++ dogs.py
@@ -26,9 +26,11 @@ def show_image():
             img_size = (int(width_spinbox.get()), int(height_spinbox.get()))
             img.thumbnail((300, 300))
             img = ImageTk.PhotoImage(img)
-            new_window = Toplevel(window)
-            new_window.title("Случайное изображение пёсика")
-            label = ttk.Label(new_window, image=img)
+            #new_window = Toplevel(window)
+            #new_window.title("Случайное изображение пёсика")
+            tab = ttk.Frame(notebook)
+            notebook.add(tab, text=f"Изображение {notebook.index('end') + 1}")
+            label = ttk.Label(tab, image=img)
             label.image = img
             label.pack(padx=10, pady=10)
 
@@ -64,6 +66,12 @@ height_label = ttk.Label(text="Высота:")
 height_label.pack(side='left', padx=(10, 0))
 height_spinbox = ttk.Spinbox(from_=200, to=500, increment=50, width=5)
 height_spinbox.pack(side='left', padx=(0, 10))
+
+top_level_window = Toplevel(window)
+top_level_window.title("Изображения пёсиков")
+
+notebook = ttk.Notebook(top_level_window)
+notebook.pack(expand=True, fill='both', padx=10, pady=10)
 
 
 window.mainloop()
